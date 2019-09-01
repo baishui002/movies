13bed859e72f18c9a430b911e5c8b22f31039d22
baishui <13809898824@139.com>
2019-09-01 21:59:24 +0800

    index

--- components/card.vue
+++ components/card.vue
@@ -1,23 +1,27 @@
 <template>
-	<view class="card">
+	<view class="card" :class="{'bottom': bottom}">
 		<view class="title">
 			<text>{{title}}</text>
+			<uni-icon type="arrowright" size="15"></uni-icon>
 		</view>
 		<scroll-view scroll-x="true" scroll-left="120">
 			<view class="scroll-view">
-				<view class="scroll-item">
-					<image src="../static/images/index_1.jpg" mode=""></image>
-					<text>骡子</text>
-					<text>骡子</text>
+				<view class="scroll-item" v-for="(item, index) in movies" :key="index">
+					<image :src="item.src" mode=""></image>
+					<view class="name">
+						<text>{{item.title}}</text>
+					</view>
+					<view class="rate">
+						<text >{{item.rate?item.rate: '暂无评分'}}分</text>
+					</view>
 				</view>
-
-
 			</view>
 		</scroll-view>
 	</view>
 </template>
 
 <script>
+	import uniIcon from "@/components/libs/uni-icon.vue"
 	export default {
 		data() {
 			return {
@@ -27,34 +31,76 @@
 		props:{
 			title: {
 				type: String,
-				default: 'hello world'
+				default: ''
 			},
 			movies: {
 				type: Array,
 				default: []
 			},
-		}
-		
+			bottom: {
+				type: Boolean,
+				default: true
+			}
+		},
+		 components: {
+			uniIcon
+		 }
 	}
 </script>
 
 <style scoped lang="scss">
+	.card {
+		margin-top: 40upx;
+		padding: 20upx 0;
+		background-color: #FFFFFF;
+		
+	}
+	.bottom {
+		border-bottom: 2upx solid #ccc;
+	}
+	.title {
+		display: flex;
+		justify-content: space-between;
+		padding: 0 10upx 20upx;
+		font-size: 30upx;
+		color: #000;
+	}
 	.scroll-view {
 		display: flex;
-		flex-wrap: nowrap;
-		padding-left: 40upx;
+		height: 400upx;
+		padding-left: 20upx;
 	}
 	.scroll-item {
 		display: flex;
 		flex-direction: column;
+		align-items: center;
 		justify-content: space-between;
-		width: 300upx;
-		margin-right: 40upx;
-		text-align: center;
+		// width: 270upx;
+		margin-right: 20upx;
 		
 		image {
-			width: 200upx;
-			height: 300upx;
+			// width: 270upx;
+			// height: 395upx;
+			width: 191upx;
+			height: 280upx;
+			box-shadow: 5px 5px 5px #ccc;
+			border-radius: 10upx;
 		}
+		
+		.name {
+			width: 191upx;
+			// padding: 10upx;
+			font-size: 28upx;
+			overflow: hidden;
+			text-overflow: ellipsis;
+			white-space: nowrap;
+			text-align: center;
+		}
+		
+		.rate {
+			font-size: 28upx;
+			color: #007AFF;
+		}
+		
 	}
 </style>
--- pages/tabBar/home/index.vue
+++ pages/tabBar/home/index.vue
@@ -1,25 +1,52 @@
 <template>
 	<view class="home">
-		 <swiper class="swiper" indicator-dots="true" autoplay="true" interval="3000" duration="500">
+		<!-- 轮播图 -->
+		 <swiper 
+		 class="swiper"
+		 indicator-dots="true" 
+		 autoplay="true" 
+		 interval="3000" 
+		 duration="500"
+		 indicator-color="red"
+		 indicator-active-color="#fff">
 			<swiper-item class="swiper-item" v-for="(item, index) in imgList" :key="index">
-				<image :src="item.src" mode="aspectFit"></image>
+				<image :src="item.src" mode=""></image>
 			</swiper-item>
 		</swiper>
-		<card ></card>
+		
+		<!-- 正在上映 -->
+		<card title="正在上映的电影" :movies="playing"></card>
+		
+		<!-- 即将上映 -->
+		<card title="即将上映的电影" :movies="coming"></card>
+		
+		<!-- 新片榜 -->
+		<card title="豆瓣电影新片榜" :movies="newRank"></card>
+		
+		<!-- Top250 -->
+		<card title="豆瓣电影Top250" :movies="top250" border="0"></card>
 	</view>
 </template>
 
 <script>
-	import { reqWeekly } from '@/api/index.js'
+	import { reqWeekly, reqNowPlaying, reqComing, reqNew, reqTop} from '@/api/index.js'
 	import card  from '@/components/card.vue'
 	export default {
 		data() {
 			return {
-				imgList: []
+				imgList: [],
+				playing: [],
+				coming: [],
+				newRank: [],
+				top250: [],
 			}
 		},
 		onLoad() {
 			this.getWeedkly()
+			this.getPlaying()
+			this.getComing()
+			this.getnewRank()
+			this.getTop250()
 		},
 		methods: {
 			// 轮播图
@@ -28,7 +55,60 @@
 				res.subjects.splice(0, 5).forEach((item) => {
 					this.imgList.push({src: item.subject.images.small, id:item.subject.id})
 				})
-			}
+			},
+			
+			// 正在热映
+			async getPlaying() {
+				const res = await reqNowPlaying()
+				res.entries.splice(0, 10).forEach((item) => {
+					this.playing.push({
+						id:item.id,
+						src: item.images.small,
+						title: item.title,
+						rate: item.rating,
+					})
+				})
+			},
+			
+			// 正在热映
+			async getComing() {
+				const res = await reqComing()
+				res.entries.splice(0, 10).forEach((item) => {
+					this.coming.push({
+						id:item.id,
+						src: item.images.small,
+						title: item.title,
+						rate: item.rating,
+					})
+				})
+			},
+			
+			// 新片榜
+			async getnewRank() {
+				const res = await reqNew()
+				res.subjects.splice(0, 10).forEach((item) => {
+					this.newRank.push({
+						id:item.id,
+						src: item.images.small,
+						title: item.title,
+						rate: item.rating.average,
+					})
+				})
+			},
+			
+			// 即将热映
+			async getTop250() {
+				const res = await reqTop()
+				res.subjects.splice(0, 10).forEach((item) => {
+					this.top250.push({
+						id:item.id,
+						src: item.images.small,
+						title: item.title,
+						rate: item.rating.average,
+					})
+				})
+			},
+			
 		},
 		components:{
 			card
@@ -37,18 +117,22 @@
 </script>
 
 <style scoped lang="scss">
-	.home {}
+	.home {
+		background-color: #F8F8F8;
+	}
 	.swiper {
-		padding-bottom: 10upx;
+		padding-bottom: 20upx;
 		width: 100%;
 		height: 400upx;
 		background-color: #F8F8F8;
 		border-bottom: 1upx solid #999999;
 		.swiper-item {
+			width: 100%;
+			height: 270upx;
 		}
-			// image {
-			// 	width: 270upx;
-			// 	height: 100%;
-			// }
+			image {
+				width: 100%;
+				height: 100%;
+			}
 	}
 </style>
